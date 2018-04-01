f959453a030709ec0cb890e243061a41729ca2ed
Daniel Schulz <me@schulzd.me>
2018-04-02 01:27:26 +1000

    Remove python 2 code

--- spectrogram.py
+++ spectrogram.py
@@ -2,7 +2,6 @@ import matplotlib
 import matplotlib.pyplot as plt, mpld3
 matplotlib.use('agg',warn=False, force=True)
 from matplotlib import pyplot as plt
-print "Switched to:",matplotlib.get_backend()
 import numpy as np
 
 a0 = 308.3544123
